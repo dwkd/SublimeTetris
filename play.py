bbf205645bb2da3663135e8880bbb5e21d38a321
Alexandru Trican <webmaster@thebluepipe.com>
2012-08-31 16:48:10 -0500

    Update Exp.003
    Experimental stage - began to add GAME OVER

--- play.py
+++ play.py
@@ -68,11 +68,22 @@ class playCommand(sublime_plugin.TextCommand):
 			maxshapes +=1
 			if maxshapes < 5:
 				self.view.run_command("play",{"gamestate":0})
+			else:
+				self.gameover()
 
 	def solidify(self):
 		global solidifiedRegions
 		solidifiedRegions += self.view.get_regions("player1")
 		self.view.add_regions("solidifiedplayer1", solidifiedRegions, "source", sublime.DRAW_EMPTY)
+
+	def gameover(self):
+		sp = self.view.find_all(" ")
+		text = ["(10, 11)","(11, 12)","(12, 13)","(13, 14)","(111, 112)","(212, 213)","(313, 314)","(414, 415)","(415, 416)","(416, 417)","(417, 418)","(316, 317)","(215, 216)","(214, 215)","(419, 420)","(318, 319)","(217, 218)","(116, 117)","(15, 16)","(16, 17)","(17, 18)","(18, 19)","(119, 120)","(220, 221)","(321, 322)","(422, 423)","(218, 219)","(219, 220)"]
+		r = []
+		for region in sp:
+			if str(region) in text:
+				r.append(region)
+		self.view.add_regions("gameoverregion", r, "source", sublime.DRAW_EMPTY)
 
 
 class resetCommand(sublime_plugin.TextCommand):
@@ -82,3 +93,4 @@ class resetCommand(sublime_plugin.TextCommand):
 		solidifiedRegions = []
 		maxshapes = 0
 		self.view.erase_regions("solidifiedplayer1")
+		self.view.erase_regions("gameoverregion")
